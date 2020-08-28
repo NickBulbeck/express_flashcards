524ffd20e1f853106328bba74925612c50efec63
NickBulbeck <bulbecknick@gmail.com>
2020-08-28 21:32:37 +0100

    Added a test folder

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
 
-console.log(express);
+const app = express();
+
+// app.get() HANDLES get-requests: it's NOT sending one!
+app.get('/', (req, res) => { // request, response - by convention
+  res.send('<h1 style="font-family: sans-serif">I love TreeHouse...</h1>');
+}); 
+
+app.get('/hello', (req, res) => { // request, response - by convention
+  res.send('<h2 style="font-family: sans-serif">Hello Nick, JS developer!</h1>');
+}); 
+
+
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost.3000');
+});
