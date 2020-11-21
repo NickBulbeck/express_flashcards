6415f78f535a5a665419abb2c75553b58aae2250
NickBulbeck <bulbecknick@gmail.com>
2020-11-21 22:03:15 +0000

    Added a question/answer href - also a failed redirect

--- routes/cards.js
+++ routes/cards.js
@@ -22,7 +22,13 @@ const cards = data.cards;								 // out of a .json file.
 // We'll also use a query string, like url?key1=value1&key2=value2
 // You check for a query string using req.query.foadybKey - req.query is a built-in property
 
-console.log("Is this file even still there?");
+// This first yin redirects the user to a random card from the basic route
+// <<website>>/cards with no id:
+router.get('/', (req,res) => {	
+	const randomID = Math.floor(Math.random() * cards.length);
+	console.log(`/cards/${randomId}?side=question`);
+	res.redirect(`/cards/${randomId}?side=question`); // 
+});													  // 
 
 
 router.get('/:id', (req, res) => {
@@ -35,12 +41,24 @@ router.get('/:id', (req, res) => {
 	}
 	const side = req.query.side || 'question';
 	const text = cards[id][side];
-	let hint = cards[id].hint;
-	if (side === 'answer') {
-		hint = null;
+	const hint = cards[id].hint;
+
+	const templateData = { id, text };
+
+	if (side === 'question') {
+		templateData.hint = hint;
+		templateData.sideToShow = 'answer';
+		templateData.sideToShowDisplay = 'Show the answer...';
+	} else if (side === 'answer') {
+		templateData.sideToShow = 'question';
+		templateData.sideToShowDisplay = 'Show the question...';
 	}
 
-	const templateData = { text,hint };
+// You can't put comments in a pug template, so note what's going on in ../views/card.pug.
+// the href for the <a> is set up as the last part of a route - that is, id followed by the
+// query string. This works because it's rendered from here, which already has the route
+// <<website>>/cards built in.
+
 	res.render('card', templateData);
 });
 
